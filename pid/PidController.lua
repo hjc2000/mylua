aba26bb9205fb2f75fbf46069e5f59f95ad6253a
hjc2000 <hjc.666@foxmail.com>
2024-08-08 10:09:40 +0800

    function PidController_Input(context, x, use_integral)

--- pid/PidController.lua
+++ pid/PidController.lua
@@ -2,26 +2,26 @@
 --- @param kp number
 --- @param ki number
 --- @param kd number
---- @param integral_separation_threshold number
---- @param output_max number
---- @param output_min number
+--- @param integral_positive_saturation number 积分正饱和
+--- @param integral_negative_saturation number 积分负饱和
 --- @return table PID控制器上下文。
 function PidController_New(kp, ki, kd,
-						   integral_separation_threshold,
-						   output_max,
-						   output_min)
+						   integral_positive_saturation,
+						   integral_negative_saturation)
 	local context = {}
 	context.kp = kp
 	context.ki = ki
 	context.kd = kd
-	context.integral_separation_threshold = integral_separation_threshold
-	context.output_max = output_max
-	context.output_min = output_min
+	context.integral_positive_saturation = integral_positive_saturation
+	context.integral_negative_saturation = integral_negative_saturation
 
 	context.x0 = 0
 	context.x1 = 0
 	context.x2 = 0
-	context.y = 0
+
+	context.p = 0
+	context.i = 0
+	context.d = 0
 
 	return context
 end
@@ -29,32 +29,29 @@ end
 --- 向PID控制器输入一个值并得到输出。
 --- @param context table PID控制器上下文。
 --- @param x number 输入值
+--- @param use_integral boolean 是否使用积分环节
 --- @return number PID输出
-function PidController_Input(context, x)
+function PidController_Input(context, x, use_integral)
 	context.x2 = context.x1
 	context.x1 = context.x0
 	context.x0 = x
 
-	local p = context.kp * (context.x0 - context.x1)
+	context.p = context.p + context.kp * (context.x0 - context.x1)
 
-	local i = 0
-	if (math.abs(context.y) >= context.integral_separation_threshold) then
-		-- 积分分离
-		i = context.ki * context.x0
+	if (use_integral) then
+		context.i = context.i + context.ki * context.x0
+
+		-- 积分饱和
+		if (context.i > context.integral_positive_saturation) then
+			context.i = context.integral_positive_saturation
+		elseif (context.i < context.integral_negative_saturation) then
+			context.i = context.integral_negative_saturation
+		end
+	else
+		context.i = 0
 	end
 
-	local d = context.kd * (context.x0 - 2 * context.x1 + context.x2)
+	context.d = context.d + context.kd * (context.x0 - 2 * context.x1 + context.x2)
 
-	context.y = p + i + d + context.y
-
-	-- 限幅并输出
-	local output = context.y
-
-	if (output > context.output_max) then
-		output = context.output_max
-	elseif (output < context.output_min) then
-		output = context.output_min
-	end
-
-	return output
+	return context.p + context.i + context.d
 end
