48f555591bed7a61cad269b7c9588e4ae2d3ade1
hjc2000 <hjc.666@foxmail.com>
2024-08-08 11:40:41 +0800

    main

--- pid/main.lua
+++ pid/main.lua
@@ -9,9 +9,7 @@ local timer1_context = Timer_New(
 	10,
 	true,
 	function()
-		-- 将更新缓存的操作放到定时器中，不要太频繁地写 flash
-		Encoder_UpdateCumulativePulseCache()
-		Transmission_UpdataFractionGear()
+
 	end
 )
 Timer_Start(timer1_context, true)
