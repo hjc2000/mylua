b78a3426041796aab302da6ccdfec1bfcac222a7
hjc2000 <hjc.666@foxmail.com>
2024-08-08 13:46:46 +0800

    if (i % 10 == 0) then 		print(y) 	end

--- pid/test.lua
+++ pid/test.lua
@@ -1,7 +1,7 @@
 dofile("pid/PidController.lua")
 dofile("pid/InertialElement.lua")
 
-local pid_controller_context = PidController_New(1000, 0.5, 0,
+local pid_controller_context = PidController_New(1000, 0.1, 0,
 	0.5, -- 积分分离阈值
 	1.5, -- 积分上限
 	-1.5 -- 积分下限
@@ -10,11 +10,13 @@ local pid_controller_context = PidController_New(1000, 0.5, 0,
 local inertial_element_context = InertialElement_New(100, 0.001)
 
 local x = 1
-local y = 0
-local e = x - y
-for i = 0, 2000 do
-	y = PidController_Input(pid_controller_context, e)
+local e = x
+for i = 0, 1000 do
+	local y = PidController_Input(pid_controller_context, e)
 	y = InertialElement_Input(inertial_element_context, y)
 	e = x - y
-	print(y)
+
+	if (i % 10 == 0) then
+		print(y)
+	end
 end
